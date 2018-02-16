a43b8bc3e75fa147d775eb2206e311186fa01bf5
LukaszWiszniewski <wisznu07@gmail.com>
2018-02-16 16:24:06 +0100

    Added ex.4 and 5

---
+++ script4.js
@@ -0,0 +1,13 @@
+const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
+const [one,tow,three,four,five,six,seven,eight,nine] = grades;
+
+let sum = 0;
+
+const average = (...args) => {
+    args.forEach(arg => {
+        sum = sum + arg;
+        console.log(`${arg} oraz suma ${sum}`)
+    });
+    console.log(`srednia to: ${sum / args.length}`)
+};
+average(one,tow,three,four,five,six,seven,eight,nine);
